09e240da84b42035e34d35d59ef5efa9a4abed5c
Barbara Reis <118932638+brareisds@users.noreply.github.com>
2024-03-20 00:04:41 -0300

    Delete SemanaDois directory

--- SemanaDois/ExercicioTres.java
+++
@@ -1,7 +0,0 @@
-public class ExercicioTres {
-    public static void main(String[] args){
-        Data data1 = new Data();
-        data1.ajustarData(11,10,2003);
-        data1.imprimirData();
-    }
-}
